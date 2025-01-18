57320f41687a79e33f1efb95bb06d41df54473b2
earoflot <quaremanish@gmail.com>
2025-01-18 13:26:07 +0530

    h

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ const express = require('express')
 // Local Module........
 const notification = require("../NotificationData.json")
 const shopping = require("../ShoppingData.json")
+const job = require("../JobData.json")
 
 const userRouter = express.Router();
 
@@ -11,6 +12,11 @@ userRouter.get('/user/api/shopping',(req,res,next)=>{
   console.log(req.url,req.method)
   return res.json(shopping.ShoppingItems);
 })
+
+userRouter.get('/user/api/job'),(req,res,next)=>{
+  console.log(req.url,req.method)
+  return res.json(job.JobIems);
+}
 
 userRouter.get('/user/api/notification',(req,res,next)=>{
   
