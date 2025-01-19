10d8e1c6f4e0e8391a59d915a59efc498cff35dc
earoflot <quaremanish@gmail.com>
2025-01-19 14:01:09 +0530

    Done

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,10 +2,14 @@
 const express = require('express')
 // Local Module........
 const notification = require("../NotificationData.json")
+const currentnotifs = require("../CurrentnotifsData.json")
 const shopping = require("../ShoppingData.json")
 const job = require("../JobData.json")
+const home = require("../homeData.json")
 
 const userRouter = express.Router();
+
+console.log(home)
 
 userRouter.get('/user/api/shopping',(req,res,next)=>{
   
@@ -18,12 +22,22 @@ userRouter.get('/user/api/job',(req,res,next)=>{
   console.log(req.url,req.method)
   return res.json(job.JobIems);
 })
+userRouter.get('/user/api/home',(req,res,next)=>{
+  
+  console.log(req.url,req.method)
+  return res.json(home.homeItems);
+})
 
 
 userRouter.get('/user/api/notification',(req,res,next)=>{
   
   console.log(req.url,req.method)
   return res.json(notification.notificationsItems);
+})
+userRouter.get('/user/api/currentnotifs',(req,res,next)=>{
+  
+  console.log(req.url,req.method)
+  return res.json(currentnotifs.currentnotifsItems);
 })
 
 module.exports = userRouter;
